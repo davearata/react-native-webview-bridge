8723abc55b9c038a1b15e17c3096b7b7aef045b8
Matthew Benjamin <mbenjamin618@gmail.com>
2017-03-23 10:38:12 -0400

    Full Screen Support

--- android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewBridgeManager.java
+++ android/src/main/java/com/github/alinz/reactnativewebviewbridge/WebViewBridgeManager.java
@@ -40,7 +40,7 @@ public class WebViewBridgeManager extends ReactWebViewManager {
     protected WebView createViewInstance(ThemedReactContext reactContext) {
         WebView root = super.createViewInstance(reactContext);
         root.addJavascriptInterface(new JavascriptBridge(root), "WebViewBridge");
-        root.setWebChromeClient(new VideoWebChromeClient(reactContext.getCurrentActivity(), webView) {
+        root.setWebChromeClient(new VideoWebChromeClient(reactContext.getCurrentActivity(), root) {
           @Override
           public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
             callback.invoke(origin, true, false);
